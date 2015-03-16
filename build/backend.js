6e0fd1cfeb586e9163bc42866dfcad03acfe0319
James Long <longster@gmail.com>
2015-03-16 10:55:51 -0400

    sourcemaps

--- build/backend.js
+++ build/backend.js
@@ -2,41 +2,41 @@ require("source-map-support").install();
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -48,11 +48,13 @@ require("source-map-support").install();
 	var express = __webpack_require__(1);
 	var index = __webpack_require__(2);
 	var page = __webpack_require__(3);
-
+	
 	var app = express();
 	app.get('/', index);
 	app.get('/page', page);
-
+	
+	foo();
+	
 	console.log("Listening on port 4000...");
 	app.listen(4000);
 
@@ -68,7 +70,7 @@ require("source-map-support").install();
 /***/ function(module, exports, __webpack_require__) {
 
 	var t = __webpack_require__(4);
-
+	
 	module.exports = function(req, res) {
 	  var arr = JSON.parse(req.query.arr || '[]');
 	  res.send(t.map(arr, function(x) { return x + 1; }));
@@ -92,3 +94,4 @@ require("source-map-support").install();
 
 /***/ }
 /******/ ]);
+//# sourceMappingURL=backend.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,11 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'backend.js'
   },
-  externals: nodeModules
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin('require("source-map-support").install();',
+                             { raw: true, entryOnly: false })
+  ],
+  devtool: 'sourcemap'
 }
