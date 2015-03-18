17c63def0687b386b0d3a7ffbc0a19d0cf62fcdc
James Long <longster@gmail.com>
2015-03-18 11:17:13 -0400

    part2b

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,45 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
+var DeepMerge = require('deep-merge');
+
+var deepmerge = DeepMerge(function(target, source, key) {
+  if(target instanceof Array) {
+    return [].concat(target, source);
+  }
+  return source;
+});
+
+// generic
+
+var defaultConfig = {
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+    ]
+  }
+};
+
+if(process.env.NODE_ENV !== 'production') {
+  defaultConfig.devtool = 'source-map';
+  defaultConfig.debug = true;
+}
+
+function config(overrides) {
+  return deepmerge(defaultConfig, overrides || {});
+}
+
+// frontend
+
+var frontendConfig = config({
+  entry: './static/js/main.js',
+  output: {
+    path: path.join(__dirname, 'static/build'),
+    filename: 'frontend.js'
+  }
+});
+
+// backend
 
 var nodeModules = {};
 fs.readdirSync('node_modules')
@@ -12,29 +51,25 @@ fs.readdirSync('node_modules')
     nodeModules[mod] = 'commonjs ' + mod;
   });
 
-var config = {
+var backendConfig = config({
   entry: './src/main.js',
   target: 'node',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'backend.js'
   },
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-    ]
-  },
   externals: nodeModules,
   plugins: [
     new webpack.IgnorePlugin(/\.(css|less)$/),
     new webpack.BannerPlugin('require("source-map-support").install();',
                              { raw: true, entryOnly: false })
-  ],
-  devtool: 'sourcemap'
-}
+  ]
+});
 
-gulp.task('backend-build', function(done) {
-  webpack(config).run(function(err, stats) {
+// tasks
+
+function onBuild(done) {
+  return function(err, stats) {
     if(err) {
       console.log('Error', err);
     }
@@ -42,5 +77,24 @@ gulp.task('backend-build', function(done) {
       console.log(stats.toString());
     }
     done();
-  });
+  }
+}
+
+gulp.task('frontend-build', function(done) {
+  webpack(frontendConfig).run(onBuild(done));
 });
+
+gulp.task('frontend-watch', function() {
+  webpack(frontendConfig).watch(onBuild(done));
+});
+
+gulp.task('backend-build', function(done) {
+  webpack(backendConfig).run(onBuild(done));
+});
+
+gulp.task('backend-watch', function(done) {
+  webpack(backendConfig).watch(onBuild(done));
+});
+
+gulp.task('build', ['frontend-build', 'backend-build']);
+gulp.task('watch', ['frontend-watch', 'backend-watch']);
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+import t from 'transducers.js';
+
+console.log(t.map([1, 2, 3], x => x + 1));
